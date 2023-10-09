995215ed0babbdab339c95a0b3674568363eee4a
Sophie  Fry <sophiefry@Sophies-MacBook-Air.local>
2023-10-09 13:07:39 -0500

    check five examples

--- unit-testing/exercises/checkFive.js
+++ unit-testing/exercises/checkFive.js
@@ -8,4 +8,19 @@ function checkFive(num){
      result = num + " is greater than 5.";
    }
    return result;
- }
+ }     
+ const test = require('../checkFive.js')
+ const checkFive = require('../checkFive.js');
+
+ describe("checkFive", function(){
+
+    test("Descriptive feedback...", function() {
+      let output = checkFive[2];
+      expect(output).toEqual("2 is less than 5.");
+      test("returns 'num is less than 5' when num < 5.", function() {
+       
+       
+     });
+    });
+
+ });
