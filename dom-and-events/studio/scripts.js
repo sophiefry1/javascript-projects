d995243c65b91f3ad76fdbf36ee5c6ecc03adb83
Sophie Fry <fryxxsop@icloud.com>
2023-10-31 15:58:23 -0500

    dom and events studio

--- dom-and-events/studio/scripts.js
+++ dom-and-events/studio/scripts.js
@@ -1,17 +1,39 @@
-<html>
-<head>
-<title>Window Load Event</title>
-<script>
-    window.addEventListener("load", function(){
-    takeOff = document
 
-    });
-</script>
+ window.addEventListener("load", function(){
 
+   const takeoffButton = document.getElementById("takeoff");
+   const landButton = document.getElementById("landing");
+   const abortMission = document.getElementById("missionAbort");
 
-</head>
-<body>
+   const flightStatus = document.getElementById("flightStatus");
+   const shuttleBackground = document.getElementById("shuttleBackground");
+   const spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
+    takeoffButton.addEventListener("click", function(){
+   
+        let shouldTakeOff = confirm("Confirm that the shuttle is ready for takeoff.");
 
+        if (shouldTakeOff){
+            flightStatus.innerHTML = "Shuttle in flight.";
+            shuttleBackground.style.backgroundColor = "blue";
+            spaceShuttleHeight.innerHTML = "10000";
+        }
+ });
+        
+ landButton.addEventListener("click", function (){
+        alert("The shuttle is landing. Landing gear engaged.");
+       flightStatus.innerHTML = "The shuttle has landed.";
+       shuttleBackground.style.backgroundColor = "green";
+       spaceShuttleHeight.innerHTML = "0"
+ });
 
-</body>
-</html>
+  abortButton.addEventListener("click", function (){
+let shouldAbort = confirm("Confirm that you want to abort the mission.");
+
+if (shouldAbort){
+   flightStatus.innerHTML = "Mission aborted";
+   shuttleBackground.style.backgroundColor = "green";
+   spaceShuttleHeight.innerHtml = "0";
+}
+ 
+})
+
